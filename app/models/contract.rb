b24aeab729856598cf42fa007db72f42b808ca76
cabelhigh <cabelhigh@yahoo.com>
2017-05-12 04:46:42 -0700

    ever day im updatin

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,3 +1,7 @@
 class Contract < ApplicationRecord
 
+
+  def format_upkeep
+    case self.upkeep; when 1; "30"; when 2; "20 and 40"; when 3; "20, 40, and 60"; when 6; "10, 20, 30, 40, 50, and 60"; end
+  end
 end
